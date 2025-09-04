3d08844dfa4ddfff41c4600783a78fa5ec7f47db
davidjprobst <probst.dj2@gmail.com>
2025-09-04 07:21:30 -0400

    Write pseudocode for getting computer answer

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello, World");
+// SET variable computerAnswer to store the computer's answer
+
+// GET computer's answer to a random answer of rock, paper, or scissors
+    // GET a random number between 0 and 99
+    // ROUND the number to the nearest whole one or two digit intiger
+    // IF the number is between greater than or equal to 0 and less than or equal to 33
+        // SET computerAnswer to "Rock"
+    // ELSE IF the number is greater than 33 and less than or equal to 66
+        //SET computerAnswer to "Paper"
+    // ELSE IF the number is greater than 66 and less than or equal to 99
+        // SET computerAnswer to "Scissors"
+    // PRINT computerAnswer to console 
