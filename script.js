7b3bbbe32a4359cf9d02fda9d8661019c1469a2f
davidjprobst <probst.dj2@gmail.com>
2025-09-25 08:37:17 -0400

    Fix score counter and end of game message
    Fix issue where playRound ran twice on new game
    Remove new game function
    Put variable definitions at global level

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanScoreCounter = document.querySelector("#human-score-counter");
+    humanScoreCounter.textContent = `Human score: ${humanScore}`;
+
+const cpuScoreCounter = document.querySelector("#cpu-score-counter");
+    cpuScoreCounter.textContent = `Computer score: ${computerScore}`;
+
+const results = document.querySelector("#results");
+const humanRoundChoice = document.createElement("p");
+const cpuRoundChoice = document.createElement("p");
+const roundResult = document.createElement("p");
+
+const btnGroup = document.querySelector("#btn-group");
+    const roundBtns = btnGroup.querySelectorAll("button");
+
+const gameEnd = document.querySelector("#game-end");
+    const newGame = document.createElement("div");
+    const gameResult = document.createElement("p");
+    const newGameBtn = document.createElement("button");
+        newGameBtn.textContent = "Start a new game";
+
 function getComputerChoice() {
     let randomNumber = Math.random() * 100;
 
@@ -12,113 +35,83 @@ function getComputerChoice() {
     return computerChoice;
 };
 
-let humanScore = 0;
-let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+    console.log("playRound ran");
 
-const humanScoreCounter = document.querySelector("#human-score-counter");
+    humanChoice = humanSelection.toLowerCase();
+    computerChoice = getComputerChoice();
+    let result = ""
+
+    if (humanChoice === "paper" && computerChoice === "rock") {
+        result = "You win! You covered the computer's rock!";
+        humanScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        result = "You win! You smashed the computer's scissors!";
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        result = "You win! You smashed the computer's scissors!";
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        result = "You lose! Your paper was cut by scissors!";
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        result = "You lose! Your scissors were smashed by a rock!";
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        result = "You lose! Your rock was covered by paper!";
+        computerScore++;
+    } else if (humanChoice === computerChoice) {
+        result = "Draw! Try again...";
+    } else {
+        result = "Something went wrong. You must choose rock, paper, or scissors.";
+    };
+
+    console.log(humanScore + " to " + computerScore);
     humanScoreCounter.textContent = `Human score: ${humanScore}`;
-
-const cpuScoreCounter = document.querySelector("#cpu-score-counter");
     cpuScoreCounter.textContent = `Computer score: ${computerScore}`;
 
-const results = document.querySelector("#results");
-    const humanRoundChoice = document.createElement("p");
-    const cpuRoundChoice = document.createElement("p");
-    const roundResult = document.createElement("p");
+    humanRoundChoice.classList.add("human-round-choice");
+    humanRoundChoice.textContent = `You chose ${humanChoice}`;
+    
+    cpuRoundChoice.classList.add("cpu-round-choice");
+    cpuRoundChoice.textContent = `The computer chose ${computerChoice}`;
+    
+    roundResult.classList.add("round-result");
+    roundResult.textContent = `${result}`;
+    
+    results.appendChild(humanRoundChoice);
+    results.appendChild(cpuRoundChoice);
+    results.appendChild(roundResult);
 
-const buttons = document.querySelectorAll("button");
+    if (computerScore === 5) {
+        gameResult.style.color = "red";
+        gameResult.textContent = "Game over! You lose :-(";
+        newGame.appendChild(gameResult);
+        newGame.appendChild(newGameBtn);
+        gameEnd.appendChild(newGame);
+    } else if (humanScore === 5) {
+        gameResult.style.color = "green";
+        gameResult.textContent = "Game over! You win :D";
+        newGame.appendChild(gameResult);
+        newGame.appendChild(newGameBtn);
+        gameEnd.appendChild(newGame);
+    }
+};
 
-const gameEnd = document.querySelector("#game-end");
-    const newGame = document.createElement("div");
-    const gameResult = document.createElement("p");
-    const newGameBtn = document.createElement("button");
-        newGameBtn.textContent = "Start a new game";
+roundBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+        humanSelection = button.id;
+        playRound();
+    });
+});
 
-
-function playGame() {
-
+newGameBtn.addEventListener("click", () => {
+    results.removeChild(humanRoundChoice);
+    results.removeChild(cpuRoundChoice);
+    results.removeChild(roundResult);
+    gameEnd.removeChild(newGame);
     humanScore = 0;
     computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-
-        humanChoice = humanSelection.toLowerCase();
-        computerChoice = getComputerChoice();
-        let result = ""
-
-        if (humanChoice === "paper" && computerChoice === "rock") {
-            result = "You win! You covered the computer's rock!";
-            humanScore++;
-            humanScoreCounter.textContent = `Human score: ${humanScore}`;
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            result = "You win! You smashed the computer's scissors!";
-            humanScore++;
-            humanScoreCounter.textContent = `Human score: ${humanScore}`;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            result = "You win! You smashed the computer's scissors!";
-            humanScore++;
-            humanScoreCounter.textContent = `Human score: ${humanScore}`;
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            result = "You lose! Your paper was cut by scissors!";
-            computerScore++;
-            cpuScoreCounter.textContent = `Computer score: ${computerScore}`;
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            result = "You lose! Your scissors were smashed by a rock!";
-            computerScore++;
-            cpuScoreCounter.textContent = `Computer score: ${computerScore}`;
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            result = "You lose! Your rock was covered by paper!";
-            computerScore++;
-            cpuScoreCounter.textContent = `Computer score: ${computerScore}`;
-        } else if (humanChoice === computerChoice) {
-            result = "Draw! Try again...";
-        } else {
-            result = "Something went wrong. You must choose rock, paper, or scissors.";
-        }
-
-        humanRoundChoice.classList.add("human-round-choice");
-        humanRoundChoice.textContent = `You chose ${humanChoice}`;
-        
-        cpuRoundChoice.classList.add("cpu-round-choice");
-        cpuRoundChoice.textContent = `The computer chose ${computerChoice}`;
-        
-        roundResult.classList.add("round-result");
-        roundResult.textContent = `${result}`;
-        
-        results.appendChild(humanRoundChoice);
-        results.appendChild(cpuRoundChoice);
-        results.appendChild(roundResult);
-
-        if (computerScore === 5) {
-            gameResult.stlye.color = "green";
-            gameResult.textContent = "Game over! You win :D";
-            newGame.appendChild(gameResult);
-            newGame.appendChild(newGameBtn);
-            gameEnd.appendChild(newGame);
-        } else if (humanScore === 5) {
-            gameResult.style.color = "red";
-            gameResult.textContent = "Game over! You lose :-(";
-            newGame.appendChild(gameResult);
-            newGame.appendChild(newGameBtn);
-            gameEnd.appendChild(newGame);
-        }
-    }
-
-    buttons.forEach((button) => {
-        button.addEventListener("click", () => {
-            humanSelection = button.id;
-            playRound();
-        });
-    });
-
-    newGameBtn.addEventListener("click", () => {
-        results.removeChild(humanRoundChoice);
-        results.removeChild(cpuRoundChoice);
-        results.removeChild(roundResult);
-        gameEnd.removeChild(newGame);
-        playGame();
-    });
-
-}
-
-playGame();
+    humanScoreCounter.textContent = `Human score: ${humanScore}`;
+    cpuScoreCounter.textContent = `Computer score: ${computerScore}`;
+});
